39dba97c91853405307727ed1f9703f962a6a289
mceme <manuelcostapnf@hotmail.com>
2018-06-17 03:01:12 +0100

    sub

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -240,10 +240,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_DIP0001].nThreshold = 50; // 50% of 100
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000924e924a21715"); // 37900
+        consensus.nMinimumChainWork = uint256S("0x00"); // unknown yet
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x0000000004f5aef732d572ff514af99a995702c92e4452c7af10858231668b1f"); // 37900
+        consensus.defaultAssumeValid = uint256S("0x00"); // unknown yet
 
         pchMessageStart[0] = 0xce;
         pchMessageStart[1] = 0xe2;
