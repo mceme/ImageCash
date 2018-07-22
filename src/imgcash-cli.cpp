21a85943e01616fa35f7c755accb528393dfaacf
mceme <manuelcostapnf@hotmail.com>
2018-07-22 03:11:10 +0100

    sub

--- src/imgcash-cli.cpp
+++ src/imgcash-cli.cpp
@@ -78,9 +78,9 @@ static int AppInitRPC(int argc, char* argv[])
         std::string strUsage = _("ImgCash Core RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
-                  "  dash-cli [options] <command> [params]  " + _("Send command to Dash Core") + "\n" +
-                  "  dash-cli [options] help                " + _("List commands") + "\n" +
-                  "  dash-cli [options] help <command>      " + _("Get help for a command") + "\n";
+                  "  imgcash-cli [options] <command> [params]  " + _("Send command to Dash Core") + "\n" +
+                  "  imgcash-cli [options] help                " + _("List commands") + "\n" +
+                  "  imgcash-cli [options] help <command>      " + _("Get help for a command") + "\n";
 
             strUsage += "\n" + HelpMessageCli();
         }
